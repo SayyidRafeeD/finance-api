cfc3c49fb012ba31da006295d26c2b74d33209b5
Sayyid RafeeD <msayyid.rafeed@gmail.com>
2025-11-20 21:48:27 +0700

    feat: joi validation

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -5,12 +5,18 @@ import {
     deleteTransaction
 } from '../controllers/transactionController.js';
 import { protect } from '../middleware/authMiddleware.js';
+import { validateRequest } from '../middleware/validationMiddleware.js';
+import { createTransactionSchema } from '../validators/transactionValidator.js';
 
 const router = express.Router();
 
 router.use(protect);
-router.route('/').get(getTransactions).post(addTransaction);
-router.route('/:id').delete(deleteTransaction);
 
+router.route('/')
+    .get(getTransactions)
+    .post(validateRequest(createTransactionSchema), addTransaction); // Pasang validator di sini
+
+router.route('/:id')
+    .delete(deleteTransaction);
 
 export default router;
